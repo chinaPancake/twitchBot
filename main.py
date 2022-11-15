9d5c8686b8cff6b1aeb678c76baa34ce7a7155f1
chinaPancake <mateusz.placek99@gmail.com>
2022-11-15 21:47:53 +0100

    First working command

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from twitchio.ext import commands
+import twitch_token
 
 class Bot(commands.Bot):
-
-    def __init__(self, token: str, *, prefix: Union[str, list, tuple, set, Callable, Coroutine], client_secret: str = None, initial_channels: Union[list, tuple, Callable] = None, heartbeat: Optional[float] = 30, retain_cache: Optional[bool] = True, **kwargs):
-        super().__init__(token, prefix=prefix, client_secret, initial_channels, heartbeat, retain_cache, **kwargs)
+    def __init__(self):
+        super().__init__(token=twitch_token.twitch_token(), prefix='?', initial_channels=['SitLetto'])
 
     async def event_ready(self):
         print(f'Logged in as | {self.nick}')
-        print(f'User id is | {self.user_id}')
-        return await super().event_ready()
-    
-    
+        print(f'Usaer id is | {self.user_id}')
+
     @commands.command()
     async def hello(self, ctx: commands.Context):
-        # send a hello back!
+        await ctx.send(f'Hello, {ctx.author.name}!')
 
-        await ctx.send(f'Hello {ctx.author.name}!')
+bot = Bot()
+bot.run()
