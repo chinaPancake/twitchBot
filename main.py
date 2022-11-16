c92037c4e329a92a8452d31494024eca185e5bb7
chinaPancake <mateusz.placek99@gmail.com>
2022-11-16 23:59:30 +0100

    a

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from twitchio.ext import commands
+from twitchio import Channel
+from twitchio import User
 import twitch_token
 
 class Bot(commands.Bot):
@@ -9,9 +11,20 @@ class Bot(commands.Bot):
         print(f'Logged in as | {self.nick}')
         print(f'Usaer id is | {self.user_id}')
 
+    # Printing hello {author.name} message
     @commands.command()
     async def hello(self, ctx: commands.Context):
         await ctx.send(f'Hello, {ctx.author.name}!')
+
+    @commands.command()
+    async def rulet(self, ctx: commands.Context):
+        get_uusers = ctx.bot.get_channel('SitLetto')
+        chatters_list = []
+        for chatters in get_uusers.chatters:
+            chatters_list.append(chatters.name)
+
+        await ctx.send(f'List of users{chatters_list}')
+
 
 bot = Bot()
 bot.run()
---
+++ skillsy.md
@@ -0,0 +1,8 @@
+todo:
+
+* losowanie użytkownika z czatu
+* mowienie miłych rzeczy o kobietach "wink wink"
+* crawler
+* ruletka o bana, za punkty kanału
+* łaczenie z botem discord (np. zrobienie losowania na twitchu = wyniki na discord)
+* 
